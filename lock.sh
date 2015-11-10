2ba48b69723e6c3b393ab0cef3a646f9bed8fed0
Unintendedsideeffects <Unintendedsideeffects@users.noreply.github.com>
2015-11-10 12:59:45 +0000

    redone for better compatibility

--- lock.sh
+++ lock.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
 #scrot, imagemagick and i3lock needed
-
 scrot /tmp/screen.png
-#this is much faster than blurring the image
-convert /tmp/screen.png -scale 10% -scale 1000% /tmp/screen.png 
+i3-msg bar mode hide
+#using convert to pixellate the screenshot taken, scale and save it to tmp
+convert /tmp/screen.png -scale 10% -scale 1000% /tmp/screen.png
 
 [[ -f $1 ]] && convert /tmp/screen.png $1 -gravity center -composite -matte /tmp/screen.png
-i3lock -u -i /tmp/screen.png
+i3lock -d -i /tmp/screen.png 
+#echo mem > /sys/power/state
 rm /tmp/screen.png
+
