6f76d04b367468b1f7b67d9f7a4a7df58c171032
Unintendedsideeffects <Unintendedsideeffects@users.noreply.github.com>
2018-11-24 16:54:47 +0000

    Removed a swap file and added pscircle.sh

---
+++ .scripts/pscircle.sh
@@ -0,0 +1,8 @@
+#Feh
+
+
+pscircle --output-width=3840 --output-height=2160 --background-color=000000 --output=/tmp/Dynamic4kbg.png
+#pscircle --output-width=1200 --output-height=1980 --background-color=000000 --output=/tmp/DynamicVerticalbg.png
+feh --bg-fill /tmp/Dynamic4kbg.png /tmp/DynamicVerticalbg.png
+rm /tmp/Dynamic4kbg.png
+rm /tmp/DynamicVerticalbg.png
