f7421d78b1a6e6e25248ad7f29cbdc08c8b3ade0
Unintendedsideeffects <Unintendedsideeffects@users.noreply.github.com>
2026-01-12 14:42:57 +0100

    organize

--- .scripts/sys-stats.sh
+++
@@ -1,9 +0,0 @@
-conky -c ~/dotfiles/rc/.conkyrc > ~/dotfiles/scripts/tmp &&
-mapfile -t sysinfos <~/.scripts/tmp &&
-var="$(echo -e "Hostname: $HOSTNAME\n"${sysinfos[0]}"\n"${sysinfos[4]}"\n"${sysinfos[1]}"\n"${sysinfos[2]}"\n"${sysinfos[3]}"\n"${sysinfos[5]}"\n"${sysinfos[6]}"\n"${sysinfos[7]}"\n"${sysinfos[8]}"\nreset yabar" | rofi -dmenu -hide-scrollbar -width -30 -p "System Status: ")"
-rm ~/.scripts/tmp
-if [ "$var" = "reset yabar" ] 
-then
-	pkill yabar
-	yabar -c ~/.config/yabar/yabar.conf & disown
-fi
